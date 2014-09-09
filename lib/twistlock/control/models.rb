8879555c6606e568b6f48df82ba35508917df044
Tinco Andringa <mail@tinco.nl>
2014-09-10 00:32:09 +0200

    Initial commit

---
+++ lib/twistlock/control/models.rb
@@ -0,0 +1,9 @@
+require "twistlock/control/models/version"
+
+module Twistlock
+  module Control
+    module Models
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/twistlock/control/models/version.rb
@@ -0,0 +1,7 @@
+module Twistlock
+  module Control
+    module Models
+      VERSION = "0.0.1"
+    end
+  end
+end
