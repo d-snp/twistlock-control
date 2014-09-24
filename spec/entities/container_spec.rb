d7069d627175d51e36873733197bbfd7a74bf74f
Tinco Andringa <mail@tinco.nl>
2014-09-25 00:33:33 +0200

    mess around with the tests a bit

--- spec/entities/container_spec.rb
+++ spec/entities/container_spec.rb
@@ -2,17 +2,22 @@ require 'spec_helper'
 
 module TwistlockControl
 	describe Container do
+		attr_reader :lprov, :container
+
+		before :each do
+			@lprov = Provisioner.new(url: 'someUrl', local: true)
+			@container = Container.new(name: 'redis', url: 'git@github.com:d-snp/redis-container.git')
+			allow(Provisioner).to receive(:local).and_return(lprov)
+		end
+
 		it "should be able to get a description" do
-			lprov = Provisioner.new(url: 'someUrl', local: true)
-			c = Container.new(name: 'redis', url: 'git@github.com:d-snp/redis-container.git')
-			expect(Provisioner).to receive(:local).and_return(lprov)
 			expect(lprov).to receive(:container_description).with('redis').and_return(
 				name: 'redis',
 				description: 'Redis cache database'
 			)
-			c.get_description
-			expect(c.description).to_not be(nil)
-			expect(c.description.name).to eq("redis")
+			container.get_description
+			expect(container.description).to_not be(nil)
+			expect(container.description.name).to eq("redis")
 		end
 	end
 end
--- spec/entities/provisioner_api_spec.rb
+++ spec/entities/provisioner_api_spec.rb
@@ -2,16 +2,25 @@ require 'spec_helper'
 
 module TwistlockControl
 	describe ProvisionerAPI do
+		attr_reader :api
+		before :all do
+			@api = ProvisionerAPI.new("localhost:3000")
+		end
 		describe '#container_description' do
-			it "should connect to the given address request the container description" do
-				api = ProvisionerAPI.new("localhost:3000")
+			before :all do
 				stub_request(:get, api.url + '/templates/redis').to_return(:body => {
 					name: 'redis',
 					description: 'a description'
 				}.to_json)
+			end
+			it "should connect to the given address request the container description" do
 				result = api.container_description("redis")
 				expect(result['name']).to eq("redis")
 			end
+		end
+
+		describe '#download_container' do
+			it 'should issue a download container request to the api'
 		end
 	end
 end
