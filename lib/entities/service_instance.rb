df8ba7a9417679cd1932fe207f34740c361ca8e7
Tinco Andringa <mail@tinco.nl>
2015-01-04 23:19:38 +0100

    write up thoughts about the ambassador system

--- lib/entities/service_instance.rb
+++ lib/entities/service_instance.rb
@@ -98,7 +98,41 @@ module TwistlockControl
 		# task that's not guaranteed to have a perfect solution.
 		#
 		# We could also for now simply assume a flat ip space, and work on the 
-		# ambassador system later.
+		# ambassador system later. A downside of that is that we might miss some
+		# architectural decision would enable the ambassador system to be more neatly
+		# integrated. So let's thing about the ambassador approach first.
+		#
+		# During the provisioning assignment step, it would become clear on which
+		# machine a process is going to be provisioned. If a linked container is
+		# on a different machine, the system detects it and links the container
+		# into an ambassador.
+		#
+		# Easiest will be to have a single ambassador per host. We could inform the
+		# ambassador of a cross-machine link via a HTTP POST, to which it would respond
+		# with the port numbers it will use for the link.
+		# Nice thing about this approach is that we can simply assume the ambassador
+		# is always there, so no complex logic for spawning it. Also it would be really
+		# easy to disable it and work with a flat ip space.
+		#
+		# So now the provisioning assignment step will be like this, for each container:
+		#    - pick a machine it will run on
+		#    - select any mounts
+		#    - configure any environment variables
+		#
+		# Then the provisioning preparation process will be:
+		#    - make sure all hosts have container descriptions/images
+		#    - determine all cross-machine links
+		#    - inform ambassadors of links
+		# 
+		# Then the provisioning process itself:
+		#    - start each container on its host
+		#    - fill in ip-addresses of container instances
+		#    - whenever a container is live, determine if any of its links can be
+		#      established, and if so establish them
+		#
+		# Basically the only thing we need to make sure in the architecture is that it can
+		# be deduced from the link information whether the link is remote or local, so
+		# the system can decide if it has to go through an ambassador.
 
 		def generate_id
 			name
